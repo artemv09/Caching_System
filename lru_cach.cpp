58273f341c88e903af5e5495befa2e2fb2076582
artemv09 <artemav.007@gmail.com>
2026-09-11 04:35:04 +0300

    написал LRU

--- lru_cach.cpp
+++ lru_cach.cpp
@@ -1,47 +1,74 @@
 #include "lru_cach.hpp"
 
-bool Lru_cach::access (int key)
+void Lru_cach::access (int key)
 {
+    if(capacity_ == 0)
+    {
+        return;
+    }
+
+    auto found_ell = hash_table.find(key);
+
+    if(found_ell != hash_table.end())
+    {
+        Lru_cach::make_recent(found_ell -> second);
+        //found_ell -> second = lru_cach.begin(); вроде не нужно
+        return;
+    }
+
+    Lru_cach::insert_new(key);
+
+    if(lru_cach.size() > capacity_)
+    {
+        Lru_cach::evict_oldest();
+        return;
+    }
+
+    return;
+}
+
+void Lru_cach::make_recent(Iterator position)// перенести существующий узел списка в head
+{
+    lru_cach.splice(lru_cach.begin(), lru_cach, position);
 
 }
 
-void Lru_cach::make_recent(Iterator position);// перенести существующий узел списка в head
+void Lru_cach::evict_oldest()// удалить самый давний элемент из списка и хеш-таблицы
 {
-
+    int key = lru_cach.back();
+    hash_table.erase(key);
+    lru_cach.pop_back();
 }
-void Lru_cach::evict_oldest();// удалить самый давний элемент из списка и хеш-таблицы
-{
 
-}
 void Lru_cach::insert_new(int key)
 {
-
+    //TODO нейронка предлагает налепить try и catch хз насколько целесообразно
+    lru_cach.push_front(key);
+    hash_table.emplace(key, lru_cach.begin());
 }
 
-Lru_cach::Lru_cach(): capacity(0), quantity_fill_elements(0), fill_flag(false)
+Lru_cach::Lru_cach(): capacity_(0)
 {
 }
 
-Lru_cach::Lru_cach(std::size_t capacity): capacity(capacity), quantity_fill_elements(0), fill_flag(true)
+Lru_cach::Lru_cach(std::size_t capacity): capacity_(capacity)
 {
 }
 
-bool Lru_cach::access(int key);
+std::size_t Lru_cach::size() const noexcept
 {
-
+    return lru_cach.size();
 }
 
-bool Lru_cach::find_key_in_hash_table(int key);
+std::size_t Lru_cach::capacity() const noexcept
 {
-
+    return capacity_;
 }
 
-void Lru_cach::addition_hash_table(int key);
+void Lru_cach::output()
 {
-
-}
-
-void Lru_cach::delete_hash_table();
-{
-
+    for(int copy : lru_cach)
+    {
+        std::cout << copy << " ";
+    }
 }
--- lru_cach.hpp
+++ lru_cach.hpp
@@ -7,6 +7,7 @@
 #include <list>
 #include <unordered_map>
 #include <iterator>
+#include <cstddef>
 
 using Iterator = std::list<int>::iterator;
 
@@ -15,24 +16,26 @@ class Lru_cach
     private:
         std::list<int> lru_cach; //сам кэш
         std::unordered_map<int, Iterator> hash_table;
-        std::size_t capacity;
-        std::size_t quantity_fill_elements;
-        bool fill_flag; // если есть свободное место то true, нет то false
-
+        std::size_t capacity_;
+        
         void make_recent(Iterator position);// перенести существующий узел списка в head
         void evict_oldest();// удалить самый давний элемент из списка и хеш-таблицы
         void insert_new(int key);// добавить новый узел списка и соответствующую запись в хеш-таблицу
-        
-        void addition_hash_table(int key);
-        void delete_hash_table();
 
     public:
         Lru_cach();
-        Lru_cach(std::size_t capacity);
+        explicit Lru_cach(std::size_t capacity);
 
-        bool access(int key);
-        bool find_key_in_hash_table(int key);
-        //TODO написать метод для вставки в хеш таблицу, удаления из хеш таблицы 
+        Lru_cach(const Lru_cach&) = delete;
+        Lru_cach& operator=(const Lru_cach&) = delete;
+
+        void access(int key); //функция для обедлинения всего в одну систему;
+
+        std::size_t size() const noexcept;
+        std::size_t capacity() const noexcept;
+
+        void output();
 };
+
 
 #endif
